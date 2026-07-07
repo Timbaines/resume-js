d1d84dbd241c84561b03ebd3e1b8826043124654
Timbaines <timbaines@timbaines.com>
2026-07-06 23:19:28 -0400

    add summary-container modifier class to summary module

--- src/sections/summary.mjs
+++ src/sections/summary.mjs
@@ -1,6 +1,6 @@
 export function renderSummary({ summary }) {
     return `
-    <div class="section-container">
+    <div class="section-container summary-container">
         <h2>Professional Summary</h2>
         <p>${summary}</p>
     </div>
